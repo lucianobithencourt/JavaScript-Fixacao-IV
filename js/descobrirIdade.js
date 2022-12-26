9a3514c003a45a95f01b52af08370a8bf12f24b0
Luciano Bitencourt <lucbite@gmail.com>
2022-12-26 17:49:18 -0300

    update
    update

--- js/descobrirIdade.js
+++ js/descobrirIdade.js
@@ -3,20 +3,98 @@
 function calcularIdade() {
 
     var anoNasc = document.getElementById('iano-nasc').value;
+    var sexo = document.getElementsByName('op-sexo');
+    
+    var nome = document.getElementById('inome').value
 
-    var anoAtual = new Date();
+    // logica calculo de idade
 
-    anoAtual.getFullYear();
+    var ano = new Date();
 
-    var idade = (anoAtual.getFullYear() - anoNasc)
+    var anoAtual = ano.getFullYear();
+    var idade = (anoAtual-anoNasc);
 
-    res.innerHTML = (`Tem ${idade} anos`);
+    var genero = ''
 
-    if (idade < 18) {
-        alert("Você é menor de idade");
+    
+
+   
+    // Condicional if 
+
+    if(sexo[0]){
+        genero = 'masculino'
+    } if (idade >=0 && idade <= 3) {
+
+        // bebê
+        msg.innerHTML = (`Analise concluida !! <br> 'Pessoa do genero ${genero} com idade de ${idade} de anos`);
+        imagem.src = '../img/bebe.png'
+
+    } else if ( idade > 4 && idade <= 11){
+        // criança
+        msg.innerHTML = (`Analise concluida !! <br> 'Pessoa do genero ${genero} com idade de ${idade} de anos`);
+        imagem.src = '../img/menino-5anos.png'
+
+       
+    } else if ( idade > 10 && idade <= 17){
+        // criança
+        msg.innerHTML = (`Analise concluida !! <br> 'Pessoa do genero ${genero} com idade de ${idade} de anos`);
+        imagem.src = '../img/adolescente-homem.png'
+
+    } else if ( idade > 18 && idade <= 29){
+        // criança
+        msg.innerHTML = (`Analise concluida !! <br> 'Pessoa do genero ${genero} com idade de ${idade} de anos`);
+        imagem.src = '../img/homem-jovem.png'
+
+    } else if ( idade > 30 && idade <= 59){
+        // criança
+        msg.innerHTML = (`Analise concluida !! <br> 'Pessoa do genero ${genero} com idade de ${idade} de anos`);
+        imagem.src = '../img/homem-adulto.png'
 
     } else {
-        alert("Você é maior de Idade");
-    }
-}
+        // criança
+        msg.innerHTML = (`Analise concluida !! <br> 'Pessoa do genero ${genero} com idade de ${idade} de anos`);
+        imagem.src = '../img/homem-idoso.png'
 
+    }
+    
+
+        // Feminino ----
+
+        if(sexo[1]){
+            genero = 'feminino'
+        } if (idade >=0 && idade <= 3) {
+    
+            // bebê
+            msg.innerHTML = (`Analise concluida !! <br> 'Pessoa do genero ${genero} com idade de ${idade} de anos`);
+            imagem.src = '../img/bebe.png'
+    
+        } else if ( idade > 4 && idade <= 11){
+            // criança
+            msg.innerHTML = (`Analise concluida !! <br> 'Pessoa do genero ${genero} com idade de ${idade} de anos`);
+            imagem.src = '../img/menina-5anos.png'
+    
+           
+        } else if ( idade > 10 && idade <= 17){
+            // criança
+            msg.innerHTML = (`Analise concluida !! <br> 'Pessoa do genero ${genero} com idade de ${idade} de anos`);
+            imagem.src = '../img/adolescente-mulher.png'
+    
+        } else if ( idade > 18 && idade <= 29){
+            // criança
+            msg.innerHTML = (`Analise concluida !! <br> 'Pessoa do genero ${genero} com idade de ${idade} de anos`);
+            imagem.src = '../img/mulher-jovem.png'
+    
+        } else if ( idade > 30 && idade <= 59){
+            // criança
+            msg.innerHTML = (`Analise concluida !! <br> 'Pessoa do genero ${genero} com idade de ${idade} de anos`);
+            imagem.src = '../img/mulher-adulta.png'
+    
+        } else {
+            // criança
+            msg.innerHTML = (`Analise concluida !! <br> 'Pessoa do genero ${genero} com idade de ${idade} de anos`);
+            imagem.src = '../img/mulher-idosa.png'
+            
+        }
+        
+
+}
